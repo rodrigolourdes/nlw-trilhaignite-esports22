e709076aed33c80552fe6addaa1374fce3ab2ded
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-15 01:10:41 -0300

    nv

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,7 +12,29 @@ function App() {
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
 
-      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient">duo</span> está aqui.</h1>
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.</h1>
+
+      <div className='grid grid-cols-6 gap-6'>
+        <a href="">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
+
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 33.57%)',
       },
     },
   },
