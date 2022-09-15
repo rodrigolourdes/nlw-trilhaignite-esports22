7e0135fd693528bd58f61a3034432ba21b7df184
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-15 02:52:58 -0300

    pause mobile dia 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
 
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
-      <Button title="Hello World" />
-      
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+
