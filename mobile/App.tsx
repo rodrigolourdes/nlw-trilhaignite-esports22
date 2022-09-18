87dae8c26ace650e6f7a599d56ecffc0a367ecb1
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-18 18:36:32 -0300

    Revert "Revert "interface full mobile""
    This reverts commit 6e62d1ab8a1af5e8e716906a62ffb70a169f13b7.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
-import { Home } from './src/screens/Home/indes';
+import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
 import { Background } from './src/components/Background'; 
 
