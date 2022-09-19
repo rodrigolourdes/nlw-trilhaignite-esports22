9db89ce8574427727eb8ec5d5887bc7d1d980231
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-18 22:19:38 -0300

    Fechando essa merda

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,27 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
-//localhost: 3333/ads
+//HTTP methods / API RESTful / HTTP Codes
 
-app.get('/ads', (request, response) => {
+// GET, POST, PUT, PATCH, DELETE
+
+/**
+ * Query: ...
+ * Route: ...
+ * Body: ...
+ */
+
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
     return response.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
@@ -12,6 +29,12 @@ app.get('/ads', (request, response) => {
         { id: 4, name: 'Anúncio 4' },
         { id: 5, name: 'Anúncio 5' },
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+
+    return response.json([])
 })
 
 app.listen(3333)
